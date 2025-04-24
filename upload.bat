c290f25f96e12f077bd52817080ba986affbaf02
damp11113 <damp51252@gmail.com>
2025-04-24 22:25:07 +0700

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,8 +1,5 @@
 @echo off
 
-title move pywindows.py to windows exclusive folder
-move /src/damp11113/pywindows.py /src/damp11113_windows/
-
 title change urllib3 to 1.26.15
 pip install urllib3==1.26.15
 
